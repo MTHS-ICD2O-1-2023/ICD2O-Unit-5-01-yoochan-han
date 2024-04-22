61ed96ed169a02d16c4a3737b880cf3e0ec35d1e
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-04-22 18:47:59 +0000

    added HTML and JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yoochan
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+function ShowAnswer() {
+const userAnswer = document.getElementById("get_UserAnswer").value
+//get user's answer
+const randomNumber = Math.floor(Math.random() * 6) + 1
+// returns a random integer from 1 to 6 into variable "randomNumber"
+if (userAnswer == randomNumber) {
+  document.getElementById("TheAnswer").innerHTML = "True!"
+}
+else {
+  document.getElementById("TheAsnwer").innerHTML = "False.."
+}
+}
